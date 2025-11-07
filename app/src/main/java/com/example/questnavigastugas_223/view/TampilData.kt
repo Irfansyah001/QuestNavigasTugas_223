589c02f97640c20d50eff399e48708bcc8d94ea3
Irfansyah001 <syahirfan545@gmail.com>
2025-11-07 19:21:59 +0700

    feat: Create TampilData composable
    Define a new Composable function `TampilData`.
    
    This function is set up to accept two lambda functions for navigation, `onKeBeranda` and `onKeFormulir`, to handle navigating to the home and form screens respectively.

--- app/src/main/java/com/example/questnavigastugas_223/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_223/view/TampilData.kt
@@ -1,3 +1,9 @@
 package com.example.questnavigastugas_223.view
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onKeBeranda: () -> Unit = {},
+    onKeFormulir: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
