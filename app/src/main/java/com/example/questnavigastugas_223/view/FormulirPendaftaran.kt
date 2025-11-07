97537301f9fa57684bed3e0f0ddc5c9696dfb5a1
Irfansyah001 <syahirfan545@gmail.com>
2025-11-07 19:46:35 +0700

    feat: Add title to registration form header
    Add a `Text` composable to the header `Card` on the registration form screen (`FormulirPendaftaran.kt`).
    
    This new text displays the title "Formulir Pendaftaran" using a string resource, with styling for font size, color, and padding.

--- app/src/main/java/com/example/questnavigastugas_223/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_223/view/FormulirPendaftaran.kt
@@ -25,3 +25,10 @@ fun FormulirPendaftaran(
                 .height(100.dp),
             colors = CardDefaults.cardColors(containerColor = Color.Black)
         ) {
+            Text(
+                modifier = Modifier.padding(top = 35.dp, start = 20.dp),
+                fontSize = 26.sp,
+                color = Color.White,
+                text = stringResource(id = R.string.formulir_pendaftaran)
+            )
+        }
